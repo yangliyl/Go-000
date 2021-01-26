d0900979e6fa5785321d26cf015aa1041d135d0a
yangli <8474363@qq.com>
2021-01-26 11:21:16 +0800

    week09

---
+++ Week09/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	ln, err := net.Listen("tcp", ":8000")
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("start tcp listen")
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Printf("listener.Accept(\"%s\") error(%v)", ln.Addr().String(), err)
+			return
+		}
+
+		go handler(conn)
+	}
+}
+
+func handler(conn net.Conn) {
+	defer conn.Close()
+
+	readCh := make(chan []byte, 10)
+	defer close(readCh)
+	sendCh := make(chan []byte, 10)
+	defer close(sendCh)
+
+	reader := bufio.NewReader(conn)
+
+	go func(conn net.Conn, readCh <-chan []byte, sendCh chan<- []byte) {
+		for msg := range readCh {
+			// TODO 解析数据包
+			log.Printf("recv client msg: %v", string(msg))
+			sendCh <- msg
+		}
+	}(conn, readCh, sendCh)
+
+	go func(conn net.Conn, sendCh <-chan []byte) {
+		for msg := range sendCh {
+			conn.Write(msg)
+		}
+	}(conn, sendCh)
+
+	for {
+		line, _, err := reader.ReadLine()
+		if err != nil {
+			if err != io.EOF {
+				log.Printf("read error: %v", err)
+			}
+			return
+		}
+		readCh <- line
+	}
+}
